d6c5a657fe1ccf1985a76156c669c5d80cad2cf4
Parbhat-Bansal <parbhat.bansal001@gmail.com>
2025-03-16 23:56:28 +0530

    Day17

---
+++ Questions/Day17.java
@@ -0,0 +1,40 @@
+
+class Solution {
+    public ArrayList<ArrayList<Integer>> levelOrder(Node root) {
+        // Your code here
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        if (root == null) 
+        {
+            return res;
+        }
+        
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        
+        while (!q.isEmpty()) 
+        {
+            int size = q.size();
+            ArrayList<Integer> level = new ArrayList<>();
+            
+            for (int i = 0; i < size; i++) 
+            {
+                Node curr = q.poll();
+                level.add(curr.data);
+                
+                if (curr.left != null) 
+                {
+                    q.add(curr.left);
+                }
+                
+                if (curr.right != null) 
+                {
+                    q.add(curr.right);
+                }
+            }
+            
+            res.add(level); // Add the current level to the result list
+        }
+        
+        return res;
+    }
+}
