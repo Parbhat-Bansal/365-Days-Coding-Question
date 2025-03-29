8ae718adcc9a5d128964097fa2cdcbc14c4ccfc8
Parbhat-Bansal <parbhat.bansal001@gmail.com>
2025-03-29 18:15:19 +0530

    Day30

---
+++ Questions/Day30.java
@@ -0,0 +1,40 @@
+// Question Link :- https://www.geeksforgeeks.org/problems/children-sum-parent/1?page=3&status=solved&sortBy=submissions
+
+
+class Solution {
+    // Function to check whether all nodes of a tree have the value 
+    // equal to the sum of their child nodes.
+    public static int isSumProperty(Node root) {
+        if (root == null) 
+        {
+            return 1; 
+        }
+
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+
+        while (!q.isEmpty()) 
+        {
+            Node curr = q.poll();
+            
+            int sum = 0;
+            if (curr.left != null) 
+            {
+                sum += curr.left.data;
+                q.add(curr.left);
+            }
+            if (curr.right != null) 
+            {
+                sum += curr.right.data;
+                q.add(curr.right);
+            }
+
+            if (sum != curr.data && (curr.left != null || curr.right != null)) 
+            {
+                return 0; // Return 0 (false) if the sum property is violated
+            }
+        }
+
+        return 1; // Return 1 (true) if all nodes satisfy the sum property
+    }
+}
